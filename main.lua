ba2a2e2b6afc134d7172857a69ed578bbf82100d
max-jellycat <max@jellycat.fr>
2021-04-12 11:17:48 +0200

    console output + display text + moving rectangle

--- main.lua
+++ main.lua
@@ -6,11 +6,20 @@
     https://github.com/salty-max
 ]]
 
+-- enable console output
+io.stdout:setvbuf("no")
+
 love.graphics.setDefaultFilter('nearest', 'nearest')
 
 require 'src/Dependencies'
 
+local x = 120
+local dir = 1
+local width = 96
+local height = 48
+
 function love.load()
+    print 'Hello World'
     love.window.setTitle(TITLE)
     math.randomseed(os.time())
 
@@ -41,11 +50,21 @@ function love.mouse.wasClicked(button)
 end
 
 function love.update(dt)
+    if x >= math.floor(VIRTUAL_WIDTH - width) or x <= 0 then
+        dir = dir * -1
+    end
+
+    x = x + (dir * 200 * dt)
+
     love.keyboard.keysPressed = {}
     love.mouse.buttonsClicked = {}
 end
 
 function love.draw()
     Push:start()
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.print("Hello World!", 8, 8)
+    love.graphics.setColor(128/255, 64/255, 1, 1)
+    love.graphics.rectangle('fill', x, VIRTUAL_HEIGHT / 2 - height / 2, width, height)
     Push:finish()
 end
