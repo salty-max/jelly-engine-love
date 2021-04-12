65af317cff60ed9624424acd718eb97e3ba5baec
max-jellycat <max@jellycat.fr>
2021-04-12 12:23:59 +0200

    added graphics

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -14,7 +14,17 @@ Timer = require 'lib/knife/timer'
 Event = require 'lib/knife/event'
 
 G_Fonts = {
-    ['small'] = love.graphics.newFont('assets/fonts/pico8.ttf', 12),
-    ['medium'] = love.graphics.newFont('assets/fonts/pico8.ttf', 24),
-    ['large'] = love.graphics.newFont('assets/fonts/pico8.ttf', 48)
+    ['small'] = love.graphics.newFont('assets/fonts/pico8.ttf', TILE_SIZE / 2),
+    ['medium'] = love.graphics.newFont('assets/fonts/pico8.ttf', TILE_SIZE),
+    ['large'] = love.graphics.newFont('assets/fonts/pico8.ttf', TILE_SIZE * 2)
+}
+
+G_Textures = {
+    ['mario'] = love.graphics.newImage('assets/graphics/mario.png'),
+    ['tiles'] = love.graphics.newImage('assets/graphics/tileset.png'),
+}
+
+G_Frames = {
+    ['mario'] = GenerateQuads(G_Textures['mario'], TILE_SIZE, TILE_SIZE),
+    ['tiles'] = GenerateQuads(G_Textures['tiles'], TILE_SIZE, TILE_SIZE),
 }
