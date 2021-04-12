521e358aed33286497a4c5dcf2004f2a3960b93a
max-jellycat <max@jellycat.fr>
2021-04-12 19:38:53 +0200

    Base class for oop behaviour

---
+++ src/Class.lua
@@ -0,0 +1,40 @@
+--[[
+    BASE CLASS
+    
+    JELLY ENGINE
+    Maxime Blanc
+    https://github.com/salty-max
+]]
+
+local Class = {}
+
+Class.__index = Class
+
+-- default implementation
+function Class:new() end
+
+-- extend base Class
+function Class:extends(type)
+    local cls = {}
+
+    cls.type = type
+    cls.__index = cls
+    cls.super = self
+
+    setmetatable(cls, self)
+    return cls
+end
+
+function Class:__call(...)
+    local instance = setmetatable({}, self)
+
+    instance:new(...)
+    
+    return instance
+end
+
+function Class:getType()
+    return self.type
+end
+
+return Class
