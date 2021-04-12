1b1e48f3d2f15a918468f7d50a9bcf82dd98d723
max-jellycat <max@jellycat.fr>
2021-04-12 13:14:32 +0200

    basic groundup

--- main.lua
+++ main.lua
@@ -13,13 +13,11 @@ love.graphics.setDefaultFilter('nearest', 'nearest')
 
 require 'src/Dependencies'
 
-local x = 120
-local dir = 1
-local width = 96
-local height = 48
+local angle = 0
+local scale = 1
+local scaleDir = 1
 
 function love.load()
-    print 'Hello World'
     love.window.setTitle(TITLE)
     math.randomseed(os.time())
 
@@ -50,11 +48,19 @@ function love.mouse.wasClicked(button)
 end
 
 function love.update(dt)
-    if x >= math.floor(VIRTUAL_WIDTH - width) or x <= 0 then
-        dir = dir * -1
+    -- avoid freeze on update pause
+    if dt > 0.036 then return end
+
+    angle = angle + 45 * dt
+    scale = scale + (scaleDir * dt)
+
+    if scale <= 0.5 then
+        scaleDir = 1
+    elseif scale >= 2 then
+        scaleDir = -1
     end
 
-    x = x + (dir * 200 * dt)
+    
 
     love.keyboard.keysPressed = {}
     love.mouse.buttonsClicked = {}
@@ -62,11 +68,18 @@ end
 
 function love.draw()
     Push:start()
-    love.graphics.setFont(G_Fonts['small'])
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.print("Hello World!", 8, 8)
-    love.graphics.setColor(128/255, 64/255, 1, 1)
-    love.graphics.rectangle('fill', x, VIRTUAL_HEIGHT / 2 - height / 2, width, height)
+    love.graphics.clear(RgbaToLove(60, 64, 198, 255))
+    love.graphics.setColor(RgbaToLove(87, 95, 207, 255))
+    love.graphics.setFont(G_Fonts['medium'])
+    love.graphics.printf('HELLO WORLD!', 0, VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 'center')
+    love.graphics.setColor(1,1,1,1)
+    love.graphics.draw(
+        G_Textures['foxy'],
+        G_Frames['foxy'][1],
+        VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2,
+        math.rad(angle), scale, scale,
+        TILE_SIZE / 2, TILE_SIZE / 2
+    )
 
     DisplayFPS()
     Push:finish()
