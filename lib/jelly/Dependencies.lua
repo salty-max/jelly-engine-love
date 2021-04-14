dca7e71a4a3316395e5d00a59d6f859c0b466fd8
max-jellycat <max@jellycat.fr>
2021-04-14 23:01:42 +0200

    added default palette

--- lib/jelly/Dependencies.lua
+++ lib/jelly/Dependencies.lua
@@ -6,10 +6,10 @@ _ = require 'lib.jelly.Util'
 Class = require('lib.jelly.modules.Class')
 Vector2 = require('lib.jelly.modules.Vector2')
 Animation = require('lib.jelly.modules.Animation')
-KeyboardManager = require('lib.jelly.modules.KeyboardManager')
-GamepadManager = require('lib.jelly.modules.GamepadManager')
+KeyboardManager = require('lib.jelly.modules.managers.KeyboardManager')
+GamepadManager = require('lib.jelly.modules.managers.GamepadManager')
 Event = require('lib.jelly.modules.Event')
 Scene = require('lib.jelly.modules.Scene')
-SceneManager = require('lib.jelly.modules.SceneManager')
+SceneManager = require('lib.jelly.modules.managers.SceneManager')
 
 Sprite = require('lib.jelly.Sprite')
